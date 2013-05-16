5f3d5612d67d9e58944fb5c26a336af02ea26b5b
Gustavo Dutra <gustavo.dutra@plataformatec.com.br>
2013-05-16 13:53:32 -0300

    Improve b.path module

--- src/modules/path.sh
+++ src/modules/path.sh
@@ -4,7 +4,7 @@
 ## If it has synlinks, it will be converted to the realpath.
 ## If it is a file, the path will also be expanded to realpath
 ## It does not return a trailling '/' for directories
-## @params path - a path to be expanded
+## @param path - a path to be expanded
 function b.path.expand () {
   local _DIR="$1" _FILE=""
   if [ -f "$1" ]; then
@@ -20,13 +20,45 @@ function b.path.expand () {
 }
 
 ## Returns true if the passed path is a directory, false otherwise
-## @params path - the path to be checked
+## @param path - the path to be checked
 function b.path.dir? () {
   test -d "$1"
 }
 
 ## Returns true if the passed path is a file, false otherwise
-## @params path - the path to be checked
+## @param path - the path to be checked
 function b.path.file? () {
   test -f "$1"
+}
+
+## Returns whether the path is a file
+## @param path - the path to be checked
+function b.path.block? () {
+  test -b "$1"
+}
+
+## Returns whether the path is readable
+## @param path - the path to be checked
+function b.path.readable? () {
+  test -r "$1"
+}
+
+## Returns whether the path is writable
+## @param path - the path to be checked
+function b.path.writable? () {
+  test -w "$1"
+}
+
+## Returns whether the path is older than another path
+## @param path - the path to be checked
+## @param another_path - the path to be checked against
+function b.path.older? () {
+  test "$1" -ot "$2"
+}
+
+## Returns whether the path is newer than another path
+## @param path - the path to be checked
+## @param another_path - the path to be checked against
+function b.path.newer? () {
+  test "$1" -nt "$2"
 }
