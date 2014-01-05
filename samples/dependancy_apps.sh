20610d9ff3ca1bfecfe9231d494947c6ae657b5d
Coder of Salvation <info@leon.vankammen.eu>
2014-01-06 00:15:14 +0100

    cleanup

--- samples/dependancy_apps.sh
+++ samples/dependancy_apps.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SCRIPT_PATH=$(realpath $0)
-#SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-#BANG_DIR=$(realpath "$SCRIPT_DIR/../src/")
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+BANG_DIR=$(realpath "$SCRIPT_DIR/../src/")
 
-#source "$BANG_DIR/bang.sh"
+source "$BANG_DIR/bang.sh"
 
-source "../src/bang.sh"
 b.module.require opt
 
 # make sure environment has the right apps installed
