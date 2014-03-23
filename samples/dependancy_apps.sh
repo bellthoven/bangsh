cb78f8de17164d0180d01df12bb0578daa8917e7
Gustavo Dutra <gustavotkg@gmail.com>
2014-03-23 18:19:15 -0300

    Improve dependency apps sample

--- samples/dependancy_apps.sh
+++ samples/dependancy_apps.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-SCRIPT_PATH=$(realpath $0)
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-BANG_DIR=$(realpath "$SCRIPT_DIR/../src/")
+SCRIPT_PATH=$(dirname $0)
 
-source "$BANG_DIR/bang.sh"
+source "$SCRIPT_PATH/../src/bang.sh"
 
-b.module.require opt
+function print_how_to_install_git () {
+  exec >&2
 
-# make sure environment has the right apps installed
-b.opt.check_required_apps "sed git"
+  echo 'Git is not installed. Install it before running the code'
+  echo
+  echo 'You may use one of the followings (or search for how to do it in your distro):'
+  echo
+  echo '$ apt-get install git'
+  echo '$ yum install git'
+  echo '$ pacman install git'
+
+  exit 2
+}
+
+# If you have `git` installed, change it in the following line to see how it works.
+b.try.do b.depends_on git
+b.catch DependencyNotMetException print_how_to_install_git
+b.try.end
+
+echo 'You have it installed!'
