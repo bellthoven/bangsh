c347fd670a1dc14f3123dc76d4c57b0cfd7cf471
Gustavo Dutra <gustavotkg@gmail.com>
2014-11-29 19:17:04 -0200

    Accept an absolute path by `bang run`

--- tasks/run.sh
+++ tasks/run.sh
@@ -3,5 +3,14 @@ btask.run.run () {
   local file="$1"
   shift
 
-  . "${working_dir}/$file"
+  # If "$file" is not an absolute path
+  if [ "${file:0:1}" != "/" ]; then
+    file="$working_dir/$file"
+  fi
+
+  if b.path.file? "$file"; then
+    . "$file"
+  else
+    b.abort "Could not run '$file' because it is not a file."
+  fi
 }
